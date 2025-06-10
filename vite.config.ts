10545e9fee5398816e3c54cbe366a255e0282c2b
Vyshnavi425610 <vyshnavi425610@gmail.com>
2025-06-10 12:07:17 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  
+  base: '/Portfolio/',  // 👈 must match your GitHub repo name
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
